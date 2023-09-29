597dd0d9438a8dfc05287776730b161ad968927a
Paulo Miguel <paulo2.0miguel@gmail.com>
2023-09-29 14:08:02 -0300

    testando o container grid interativo

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded"),(()=>{
+const cabelo = document.getElementById("cabelo")
+const barba = document.getElementById("barba")
+const cabeloB = document.getElementById("cabeloB")
+
+cabelo.addEventListener("click",()=>{
+    alert("clicou")
+})
+
+})
