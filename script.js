ff4f48bb66056b818a3880ce93f2fb7334ebe963
Paulo Miguel <paulo2.0miguel@gmail.com>
2023-09-30 20:17:42 -0300

    testando container interativo

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ document.addEventListener("DOMContentLoaded", () => {
     cabelo.classList.remove("containerGridSec2")
       barba.classList.remove("containerGridPrincipal")
       cabeloB.classList.remove("containerGridSec1")
+
+      testeCabelo()
     });
   
     barba.addEventListener("click", () => {
@@ -60,8 +62,177 @@ document.addEventListener("DOMContentLoaded", () => {
         cabeloB.classList.remove("containerGridSec2")
         cabelo.classList.remove("containerGridPrincipal")
         barba.classList.remove("containerGridSec1")
-
-
       });
+        
+    const tituloC = document.getElementById("tituloC")
+    const preçoC = document.getElementById("preçoC")
+    const pMesC = document.getElementById("pMesC")
+    const assinarAgoraC = document.getElementById("assinarAgoraC")
+    const botaoMaisInfoC = document.getElementById("botaoMaisInfoC")
+    const containerPrincDirC = document.getElementById("containerPrincDirC")
+    const containerInicialC = document.getElementById("containerInicialC")
+
+    const tituloB = document.getElementById("tituloC")
+    const preçoB = document.getElementById("preçoC")
+    const pMesB = document.getElementById("pMesC")
+    const assinarAgoraB = document.getElementById("assinarAgoraC")
+    const botaoMaisInfoB = document.getElementById("botaoMaisInfoC")
+    const containerPrincDirB = document.getElementById("containerPrincDirC")
+    const containerInicialB = document.getElementById("containerInicialC")
+
+    const tituloBC = document.getElementById("tituloC")
+    const preçoBC = document.getElementById("preçoC")
+    const pMesBC = document.getElementById("pMesC")
+    const assinarAgoraBC = document.getElementById("assinarAgoraC")
+    const botaoMaisInfoBC = document.getElementById("botaoMaisInfoC")
+    const containerPrincDirBC = document.getElementById("containerPrincDirC")
+    const containerInicialBC = document.getElementById("containerInicialC")
+//criar o comparador que se identificar que é o principal ele alterara os displays
+cabelo.addEventListener("click",()=>{
+if(cabelo.classList.contains("containerGridPrincipal")){
+tituloC.style.display="block"
+preçoC.style.display="block"
+pMesC.style.display="flex"
+assinarAgoraC.style.display="flex"
+botaoMaisInfoC.style.display="none"
+containerPrincDirC.style.display="block"
+
+// tituloB.style.display="none"
+// preçoB.style.display="none"
+// pMesB.style.display="none"
+// assinarAgoraB.style.display="none"
+// botaoMaisInfoB.style.display="none"
+// containerPrincDirB.style.display="none"
+
+// tituloBC.style.display="none"
+// preçoBC.style.display="none"
+// pMesBC.style.display="none"
+// assinarAgoraBC.style.display="none"
+// botaoMaisInfoBC.style.display="none"
+// containerPrincDirBC.style.display="none"
+alert("CABELO1")
+
+}
+else if(cabelo.classList.contains("containerGridSec1")){
+  tituloC.style.display="none"
+  preçoC.style.display="none"
+  pMesC.style.display="none"
+  assinarAgoraC.style.display="none"
+  botaoMaisInfoC.style.display="none"
+  containerPrincDirC.style.display="none"
+  alert("CABELO2")
+  }
+else if(cabelo.classList.contains("containerGridSec2")){
+    tituloC.style.display="none"
+    preçoC.style.display="none"
+    pMesC.style.display="none"
+    assinarAgoraC.style.display="none"
+    botaoMaisInfoC.style.display="none"
+    containerPrincDirC.style.display="none"
+    alert("CABELO3")
+    }
+else{
+  alert("erro")
+}
+})
+
+
+barba.addEventListener("click",()=>{
+if(barba.classList.contains("containerGridPrincipal")){
+  tituloB.style.display="block"
+  preçoB.style.display="block"
+  pMesB.style.display="flex"
+  assinarAgoraB.style.display="flex"
+  botaoMaisInfoB.style.display="none"
+  containerPrincDirB.style.display="block"
+  alert("BARBA1")
+  }
+  else if(barba.classList.contains("containerGridSec1")){
+    tituloB.style.display="none"
+    preçoB.style.display="none"
+    pMesB.style.display="none"
+    assinarAgoraB.style.display="none"
+    botaoMaisInfoB.style.display="none"
+    containerPrincDirB.style.display="none"
+    alert("BARBA2")
+    }
+  else if(barba.classList.contains("containerGridSec2")){
+      tituloB.style.display="none"
+      preçoB.style.display="none"
+      pMesB.style.display="none"
+      assinarAgoraB.style.display="none"
+      botaoMaisInfoB.style.display="none"
+      containerPrincDirB.style.display="none"
+      alert("BARBA3")
+      }
+  else{
+    alert("erro")
+  }
+      })
+
+
+
+      cabeloB.addEventListener("click",()=>{    
+  if(cabeloB.classList.contains("containerGridPrincipal")){
+    tituloBC.style.display="block"
+    preçoBC.style.display="block"
+    pMesBC.style.display="flex"
+    assinarAgoraBC.style.display="flex"
+    botaoMaisInfoBC.style.display="none"
+    containerPrincDirBC.style.display="block"
+    alert("CABELOB1")
+    }
+    else if(cabeloB.classList.contains("containerGridSec1")){
+      tituloBC.style.display="none"
+      preçoBC.style.display="none"
+      pMesBC.style.display="none"
+      assinarAgoraBC.style.display="none"
+      botaoMaisInfoBC.style.display="none"
+      containerPrincDirBC.style.display="none"
+      alert("CABELOB2")
+      }
+    else if(cabeloB.classList.contains("containerGridSec2")){
+        tituloBC.style.display="none"
+        preçoBC.style.display="none"
+        pMesBC.style.display="none"
+        assinarAgoraBC.style.display="none"
+        botaoMaisInfoBC.style.display="none"
+        containerPrincDirBC.style.display="none"
+        alert("CABELOB3")
+        }
+    else{
+      alert("erro")
+    }
+      })
+  
+
+
+
+
+
+
+//inicio
+  // esses ficam off no inicio
+  preço 
+  pMes
+  containerPrincDir
+
+
+
+  //container principal fica off
+  botaoMaisInfo
+
+  //container principal fica on
+  preço
+pMes
+containerPrincDir
+
+  // fica off fica sec1 e sec2
+  titulo
+  preço
+  pMes
+  assinarAgora
+  botaoMaisInfo
+  containerPrincDir
 });
 
